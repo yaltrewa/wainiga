6872c4c565facda43b3c2d615fd8f016dbc1a236
yaltrewa <misdiasnormales@gmail.com>
2025-01-21 20:13:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -57,12 +57,11 @@ function updateClock() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    clock.textContent = `${hours}:${minutes}:${seconds}`;
+    clock.textContent = `${hours}:${minutes}`;
 }
 
-// Actualiza el reloj inicialmente y cada segundo
+// Actualiza el reloj inicialmente y cada minuto
 updateClock();
-setInterval(updateClock, 1000);
+setInterval(updateClock, 60000); // Actualiza cada 60 segundos
 
 
